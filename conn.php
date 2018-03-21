5f4d7a9308a6830fe19a9e5f5a5c773f5b48b68d
gaoduoh <2207222067@qq.com>
2018-03-22 01:46:31 +0800

    database

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $dbhost = 'localhost:3306';  // mysql服务器主机地址
 $dbuser = 'root';            // mysql用户名
 $dbpass = '12345678';          // mysql用户名密码
-//$mysql_database='demo';
+//$mysql_database='generaleducation';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 if(! $conn )
 {
